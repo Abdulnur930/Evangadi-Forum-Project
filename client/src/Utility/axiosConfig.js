7fd4645c14da9e6d280aa0f59c352404b836b7f0
Abdulnur930 <hussenabdulnur@gmail.com>
2024-03-21 22:26:03 +0300

    deploy

--- client/src/Utility/axiosConfig.js
+++ client/src/Utility/axiosConfig.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   //local host
-  baseURL: "http://localhost:5550/api", 
+  // baseURL: "http://localhost:5550/api", 
   //deployed version
-// baseURL: "https://evangadi-forum-backend-v25r.onrender.com/api",
+baseURL: "https://evangadi-forum-backend-v25r.onrender.com/api",
 });
 
 export default axiosBase;
